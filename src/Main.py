6e4ae94da3a9703e8905193697a86a6ce05fb40a
Sulthon-Baiquni <sulthonbaiquni05@gmail.com>
2022-04-18 11:47:32 +0700

    pppp

---
+++ src/Main.py
@@ -0,0 +1,19 @@
+# latihan konverensi satuan temperature
+# dari celcius ke yang lain
+
+print("\nPROGRAM KONVERENSI TEMPERATURE\n")
+
+celcius = float(input('masukan suhu dalam celcius : '))
+print("suhu dalam celcius adalah",celcius, "Celcius")
+
+# reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah",reamur, "Reamur")
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah",fahrenheit, "Fahrenheit")
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin, "Kelvin")
